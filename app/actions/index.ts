6ebe9957f3ac41cfb75c9bc56ebfa238abdd93df
Tasnimul Hasan <2408020@student.ruet.ac.bd>
2026-07-15 07:37:08 +0600

    FEAT: Define actions for profile-reducer

---
+++ app/actions/index.ts
@@ -0,0 +1,9 @@
+const actions = {
+  profile: {
+    DATA_FETCHING: "PROFILE_DATA_FETCHING",
+    DATA_FETCH_ERROR: "PROFILE_DATA_FETCH_ERROR",
+    DATA_FETCHED: "PROFILE_DATA_FETCHED",
+  },
+};
+
+export default actions;
--- app/reducers/profile-reducer.ts
+++ app/reducers/profile-reducer.ts
@@ -1,21 +1,34 @@
+import actions from "~/actions";
 import type { UserProfile } from "~/types";
 
-type stateType = {
+type StateType = {
   user: UserProfile | null;
   loading: boolean;
   error: Error | null;
 };
 
-const initialState: stateType = {
+type ActionType = {
+  type: (typeof actions.profile)[keyof typeof actions.profile];
+  payload?: any;
+  error?: Error;
+};
+
+const initialState: StateType = {
   user: null,
   loading: false,
   error: null,
 };
 
-const profileReducer = (state: stateType, action: { type: string }) => {
+const profileReducer = (state: StateType, action: ActionType) => {
   switch (action.type) {
-    case "":
-      break;
+    case actions.profile.DATA_FETCHING:
+      return { ...state, loading: true };
+
+    case actions.profile.DATA_FETCH_ERROR:
+      return { ...state, loading: false, error: action.error };
+
+    case actions.profile.DATA_FETCHED:
+      return { ...state, loading: false, user: action.payload };
 
     default:
       return state;
